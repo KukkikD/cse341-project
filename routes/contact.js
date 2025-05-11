6e4dbbc843a6576a8c832516c6288ea59cc36f43
KukkikD <adeezon9@gmail.com>
2025-05-11 03:06:28 -0700

    updated

--- routes/contact.js
+++ routes/contact.js
@@ -17,4 +17,4 @@ router.put('/:id', contactController.updateContact);
 // DELETE a contact
 router.delete('/:id', contactController.deleteContact);
 
-module.exports = router;
+module.exports = router; 
